31009e74e9b0bacf3caad6084ba5d44a2d0df270
John Doe <MY_NAME@example.com>
2019-07-10 14:22:13 +0200

    ussing crontab to update jenkins plugins

---
+++ app/data/jenkins/config/jenkins_update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Author: Sharad Chhetri
+# Description: Update all Jenkins plugin
+# Version 1.0
+
+_JENKINS_URL=http://docker_jenkins:8080/jenkins
+_JENKINS_USER=admin
+_JENKINS_PASSWD=admin
+
+#copy file to tmp
+cd /
+cp -n ./var/jenkins_home/war/WEB-INF/jenkins-cli.jar /tmp
+cd /tmp
+java -jar jenkins-cli.jar -s "$_JENKINS_URL" -auth "$_JENKINS_USER:$_JENKINS_PASSWD" -noKeyAuth list-plugins|  grep -e ')$' | awk '{ print $1 }'| while read _UPDATE_LIST;
+do 
+java -jar jenkins-cli.jar -s "$_JENKINS_URL" -auth "$_JENKINS_USER:$_JENKINS_PASSWD" -noKeyAuth install-plugin "$_UPDATE_LIST";
+done
+
+java -jar jenkins-cli.jar -s "$_JENKINS_URL" -auth "$_JENKINS_USER:$_JENKINS_PASSWD" -noKeyAuth safe-restart;
