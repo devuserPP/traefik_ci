0f308dea862f60fb381f4351e1e63ebe5e2ec35a
John Doe <MY_NAME@example.com>
2019-07-15 10:24:23 +0200

    add condition only after installing new plugins will be jenksins restarted

--- app/data/jenkins/config/jenkins_update.sh
+++ app/data/jenkins/config/jenkins_update.sh
@@ -7,6 +7,10 @@ _JENKINS_URL=http://docker_jenkins:8080/jenkins
 _JENKINS_USER=admin
 _JENKINS_PASSWD=admin
 
+#test command
+#$java -jar jenkins-cli.jar -s http://docker_jenkins:8080/jenkins -auth admin:admin -noKeyAuth list-plugins|  grep -e ')$' | awk '{ print $1 }'
+
+
 #copy file to tmp
 cd /
 cp -n ./var/jenkins_home/war/WEB-INF/jenkins-cli.jar /tmp
@@ -18,4 +22,10 @@ java -jar jenkins-cli.jar -s "$_JENKINS_URL" -auth "$_JENKINS_USER:$_JENKINS_PAS
 sleep 10;
 done
 
-java -jar jenkins-cli.jar -s "$_JENKINS_URL" -auth "$_JENKINS_USER:$_JENKINS_PASSWD" -noKeyAuth safe-restart;
+if [ -z "$_UPDATE_LIST" ] ;
+then
+    echo "Nothing to do, all plugins are updated";
+else
+    java -jar jenkins-cli.jar -s "$_JENKINS_URL" -auth "$_JENKINS_USER:$_JENKINS_PASSWD" -noKeyAuth safe-restart;
+    echo "Following plugins was updated \$_JENKINS_URL";
+fi
